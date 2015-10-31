06c2ef101c935b9ae6edda143e81c43d455fb49e
junaid <junaid.jp@gmail.com>
2015-10-31 13:31:20 -0500

    Chaned Abdullahnew Test class to add junaid as the name for String

--- src/com/training/concepts/AbdullahNewTest.java
+++ src/com/training/concepts/AbdullahNewTest.java
@@ -2,4 +2,10 @@ package com.training.concepts;
 
 public class AbdullahNewTest {
 
+	
+	
+	public static void main(String args[]) { 
+		String name = "junaid"; 
+		System.out.println("NAme is" + name);
+	}
 }
