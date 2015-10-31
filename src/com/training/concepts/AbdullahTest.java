b6d835b674f27bbcbd830a7204e255a7ba9afe92
Sam <tonofal@yahoo.com>
2015-10-31 13:35:09 -0500

    change the class

--- src/com/training/concepts/AbdullahTest.java
+++ src/com/training/concepts/AbdullahTest.java
@@ -4,7 +4,8 @@ public class AbdullahTest {
 	
 	public static void main(String args[]){
 	System.out.println("New class promoted it to GIT HUB");
-	
+	String name = "Abdullah";
+	System.out.println("My name is " + name);
 	
 	
 	}
