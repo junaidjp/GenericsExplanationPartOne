8e356e5bab8f094ea633f3d9822fa0ddf188f181
Danish Ahmed <ahmed50406@gmail.com>
2015-10-31 13:39:19 -0500

    added new class

---
+++ src/com/training/concepts/DanishNewAhmed.java
@@ -0,0 +1,14 @@
+package com.training.concepts;
+
+public class DanishNewAhmed {
+public static void main (String args[]){
+		
+		
+		
+	String firstname = "Danish";
+		System.out.println("October31 practic it to GIT HUB and my name is" + firstname);
+		
+	
+	}
+}
+
