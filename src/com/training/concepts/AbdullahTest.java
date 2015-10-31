436c5a327d116397a34afa98f06328c273a6db14
Abdullah <aamamun0007@gmail.com>
2015-10-31 12:22:37 -0500

    Added my new Class by Abdullah

---
+++ src/com/training/concepts/AbdullahTest.java
@@ -0,0 +1,12 @@
+package com.training.concepts;
+
+public class AbdullahTest {
+	
+	public static void main(String args[]){
+	System.out.println("New class promoted it to GIT HUB");
+	
+	
+	
+	}
+
+}
